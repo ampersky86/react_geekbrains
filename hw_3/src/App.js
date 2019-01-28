34297aa2ff58ea61f80a8516fc8a88393e464202
ampersky86 <gasttello@gmail.com>
2019-01-29 02:53:31 +0300

    Заимпортирован Login.js

---
+++ hw_3/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Login from "./components/Login";
+
+class App extends React.Component {
+    render () {
+        return(
+            <div>
+                <Login/>
+            </div>
+        )
+    }
+}
