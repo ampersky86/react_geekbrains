8611eb862469e6c57fc17060a64223b4be3422ee
ampersky86 <gasttello@gmail.com>
2019-01-29 10:45:35 +0300

    Не работает Login

--- hw_3/src/App.js
+++ hw_3/src/App.js
@@ -4,7 +4,7 @@ import Login from "./components/Login";
 class App extends React.Component {
     render () {
         return <div>
-            <Login/>
+            <Login />
         </div>
 
     }
--- hw_3/src/index.js
+++ hw_3/src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import App from "./App2";
+import App from "./App";
 
 
 
